cbe70628eca6a6aa2c83e8dbcd5721395d0989c5
Martin Flores <cachopunk@gmail.com>
2012-10-06 18:15:02 -0300

    <<Negocios>> Consulta de todos los usuarios

--- GeoRed/source/Negocios/source/persistencia/UsuarioDAOImpl.java
+++ GeoRed/source/Negocios/source/persistencia/UsuarioDAOImpl.java
@@ -1,11 +1,24 @@
 package persistencia;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
+
+@Stateless
 public class UsuarioDAOImpl extends BaseDAO<Usuario> implements UsuarioDAO{
 
 	@Override
 	public Usuario buscarPorId(int id) {
 		Usuario ret = em.find(Usuario.class, id);
 		return ret;
+	}
+
+	@Override
+	public List<Usuario> obtenerTodos() {
+		TypedQuery<Usuario> ret = em.createQuery(
+				"SELECT u from USUARIOS u", Usuario.class);
+		return ret.getResultList();
 	}
 	
 
