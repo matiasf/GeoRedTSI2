95d364579846066816860b69f834ec5d3632f806
b3rch <b3rch>
2012-10-14 22:47:54 -0200

    cambios de berch

--- GeoRed/source/android-rest/src/com/geored/gui/LoginActivity.java
+++ GeoRed/source/android-rest/src/com/geored/gui/LoginActivity.java
@@ -5,6 +5,7 @@ import com.geored.rest.ServicioRestAutenticacion;
 import com.geored.rest.exception.RestBlowUpException;
 import com.geored.rest.exception.UnauthorizedException;
 
+import android.os.AsyncTask;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,36 +16,41 @@ public class LoginActivity extends GenericActivity {
     }
 
     public void showLogin(View clickedButton) {
-    	if (doSomething()){
-    		goToActivity(UsuarioActivity.class);
-    	}        
-    }
-    
-    private boolean doSomething(){
-    	try{
-    		String emailText = ((EditText)findViewById(R.id.emailEditText)).getText().toString();
-        	String passwordText = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
-        	
-        	usuarioId = ServicioRestAutenticacion.login(emailText, passwordText);        	
-    		
-        	showToast( usuarioId );
-    	}catch(RestBlowUpException exbu){
-    		
-    		showToast("El servicio no responde");
-        	
-    		return false;
-    	}catch(UnauthorizedException exu){
-    		
-    		showToast("El usuario no esta autorizado");
-    		
-    		return false;
-    	}catch(Exception ex){    		
-    		showToast(ex.getMessage());
-    		return false;
-    	}
+    	String emailText = ((EditText)findViewById(R.id.emailEditText)).getText().toString();
+    	String passwordText = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
     	
-    	return true;
+    	RegistryAsyncTask task = new RegistryAsyncTask();
+		task.execute(new String[] { emailText,passwordText});
     }
     
+    private String login(String emailText,String passwordText) throws RestBlowUpException, UnauthorizedException{
+    		
+        	return ServicioRestAutenticacion.login(emailText, passwordText);        	
+    		
+    }
+	private class RegistryAsyncTask extends AsyncTask<String, Void, String> {
+	    @Override
+	    protected String doInBackground(String... params) {
+	      try {
+			usuarioId = login(params[0],params[1]);
+		} catch (RestBlowUpException e) {
+			e.printStackTrace();
+			return "El servicio no responde";
+		} catch (UnauthorizedException e) {
+			e.printStackTrace();
+			return "El usuario no esta autorizado";
+		}
+	   		return "Exito";
+	    }
+	
+	    @Override
+	    protected void onPostExecute(String result) {
+	    	if (result.equals("Exito")){
+	    		goToActivity(UsuarioActivity.class);
+	    	}else{
+	    		showToast(result);
+	    	}	    	
+	    }
+	  }
     
 }
