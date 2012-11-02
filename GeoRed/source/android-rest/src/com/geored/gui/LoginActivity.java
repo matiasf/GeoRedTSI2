dd1772929756af1c40ce664e90701d86ffbbdb9b
Matias Fernandez <fernandez.matias@arvatosystems.com>
2012-11-02 16:45:55 -0200

    * Cambio float a double en PosiiconJSON. * Mejoro login Facebook. * Mejoro GCM.

--- GeoRed/source/android-rest/src/com/geored/gui/LoginActivity.java
+++ GeoRed/source/android-rest/src/com/geored/gui/LoginActivity.java
@@ -6,50 +6,54 @@ import com.geored.rest.exception.RestBlowUpException;
 import com.geored.rest.exception.UnauthorizedException;
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class LoginActivity extends GenericActivity {
 
 	protected void loadVista() {
-    	setContentView(R.layout.activity_login);
-    }
-
-    public void showLogin(View clickedButton) {
-    	String emailText = ((EditText)findViewById(R.id.emailEditText)).getText().toString();
-    	String passwordText = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
-    	
-    	RegistryAsyncTask task = new RegistryAsyncTask();
-		task.execute(new String[] { emailText,passwordText});
-    }
-    
-    private String login(String emailText,String passwordText) throws RestBlowUpException, UnauthorizedException {    		
-        	return ServicioRestAutenticacion.login(emailText, passwordText);    		
-    }
-    
-	private class RegistryAsyncTask extends AsyncTask<String, Void, String> {
-	    @Override
-	    protected String doInBackground(String... params) {
-	      try {
-			usuarioId = login(params[0],params[1]);
-		} catch (RestBlowUpException e) {
-			e.printStackTrace();
-			return "El servicio no responde";
-		} catch (UnauthorizedException e) {
-			e.printStackTrace();
-			return "El usuario no esta autorizado";
-		}
-	   		return "Exito";
-	    }
-	
-	    @Override
-	    protected void onPostExecute(String result) {
-	    	if (result.equals("Exito")){
-	    		goToActivity(UsuarioActivity.class);
-	    	}else{
-	    		showToast(result);
-	    	}	    	
-	    }
+		setContentView(R.layout.activity_login);
 	}
-    
+
+	public void showLogin(View clickedButton) {
+		String emailText = ((EditText) findViewById(R.id.emailEditText))
+				.getText().toString();
+		String passwordText = ((EditText) findViewById(R.id.passwordEditText))
+				.getText().toString();
+
+		RegistryAsyncTask task = new RegistryAsyncTask();
+		task.execute(new String[] { emailText, passwordText });
+	}
+
+	private String login(String emailText, String passwordText)
+			throws RestBlowUpException, UnauthorizedException {
+		return ServicioRestAutenticacion.login(emailText, passwordText);
+	}
+
+	private class RegistryAsyncTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				usuarioId = login(params[0], params[1]);
+			} catch (RestBlowUpException e) {
+				Log.e("Error", "Rest blow up!", e);
+				return "Rest blow up!";
+			} catch (UnauthorizedException e) {
+				Log.w("Warning", "Unautorized!", e);
+				return "Unautorized!";
+			}
+			return "Exito";
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			if (result.equals("Exito")) {
+				goToActivity(UsuarioActivity.class);
+			} else {
+				showToast(result);
+			}
+		}
+	}
+
 }
