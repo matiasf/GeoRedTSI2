724883c0866ece86c7856521891d1d0bee96f7aa
leotuerto <leotuerto@la-dell-leo.(none)>
2012-11-03 13:33:33 -0200

    mapa centra y no hace el marker

---
+++ GeoRed/source/richfaces-geored-web/src/main/java/javaBB/DatoReporteCheckIn.java
@@ -0,0 +1,59 @@
+package javaBB;
+
+
+public class DatoReporteCheckIn {
+
+	   
+	
+	public String nombre;	
+	public String descripcion;	
+	public double latitud;
+	public double longitud;
+	public int cantidad;
+	
+	public DatoReporteCheckIn(String nombre, String descripcion,
+			double latitud, double longitud, int cantidad) {	
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.latitud = latitud;
+		this.longitud = longitud;
+		this.cantidad = cantidad;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getLatitud() {
+		return latitud;
+	}
+	public void setLatitud(double latitud) {
+		this.latitud = latitud;
+	}
+	public double getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(double longitud) {
+		this.longitud = longitud;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
+
+	
+	
+	
+}
--- GeoRed/source/richfaces-geored-web/src/main/webapp/resources/js/cargarMapa.js
+++ GeoRed/source/richfaces-geored-web/src/main/webapp/resources/js/cargarMapa.js
@@ -12,6 +12,23 @@
         placeMarker(event.latLng);
       });
     }
+    
+    
+    function initializeConPunto() {
+    	var lat = document.getElementById("form:coordX").value;
+    	var len = document.getElementById("form:coordY").value;
+        var myLatlng = new google.maps.LatLng(lat,len);
+        var mapOptions = {
+          zoom: 17,
+          center: myLatlng,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);      
+        map.markers = new Array();
+        google.maps.event.addListener(map, 'click', function(event) {    	
+          placeMarker(event.latLng);
+        });
+      }
 
     function placeMarker(location) {      
       //alert(document.getElementById("form").getAttribute("coordX"));      
@@ -29,4 +46,32 @@
       map.markers[0] = marker;
       map.setCenter(location);
     }
+    
+    function cargarPuntoUnico() {    	  
+    	  var lat = document.getElementById("form:coordX").value;
+    	  var len = document.getElementById("form:coordY").value;
+    	  var centro = new google.maps.LatLng(lat,len);
+    	  placeMarker(centro);
+    }
+    
+    function placeMarkers(location) {     
+        alert("llame a place Markers con " + location);
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+        //for(var i=0; i<map.markers.length; i++){
+    	//	map.markers[i].setMap(null);
+        //}
+        if (map.markers==null){
+        	map.markers = new Array();
+        }        
+        map.markers[map.markers.length] = marker;
+        map.setCenter(location);
+      }
+    
+    function cargarPuntoIterativo(lat, len) {    	  
+  	  var centro = new google.maps.LatLng(lat,len);
+  	  placeMarkers(centro);
+  }
     
